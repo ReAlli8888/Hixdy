194b956e2672135f2779a3f512f3caca0455b727
ReAlli8888 <45660768+ReAlli8888@users.noreply.github.com>
2019-01-15 15:35:59 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("هنا اي دي الروم")
+setInterval(function() {
+channel.send(`هنا كلام ال سبام`);
+}, 30)
+})
+
+client.login(process.env.TOKEN);
+client2.login(process.env.TOKEN2);
